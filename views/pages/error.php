5242bc50210a16a5048452100fb0c678a09f7e4a
Yoli_kks <527045740@qq.com>
2018-08-04 20:00:31 +0800

    Add files via upload

---
+++ views/pages/error.php
@@ -0,0 +1,13 @@
+<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+		<div class="wrap-index p-l-55 p-r-55 p-t-80 p-b-30">
+			<span class="index-form-bigtitle p-b-75">
+					Musicaction
+			</span>
+			<form class="login100-form validate-form p-b-20">
+				<div class="txt2">
+					Looks like something went wrong.
+				</div>
+			</form>
+		</div>
+	</div>
+	
