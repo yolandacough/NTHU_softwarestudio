4a391159a5874855527af1edc2e7ec1c2167401d
Yoli_kks <527045740@qq.com>
2018-08-04 19:59:56 +0800

    Create home.php

---
+++ views/pages/home.php
@@ -0,0 +1,24 @@
+<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+		<div class="wrap-index p-l-55 p-r-55 p-t-80 p-b-30">
+			<span class="index-form-bigtitle p-b-75">
+					Musicaction
+			</span>
+			<form class="login100-form validate-form">
+				<div class="flex-c p-b-50">
+				<div class="container-login100-form-btn">
+					<a href="?controller=accounts&action=signin"  class="login100-form-btn">
+						Sign In
+					</a> 
+				</div>
+
+				<div class="container-login100-form-btn">
+					<a href="?controller=accounts&action=signup" class="login100-form-btn">
+						Sign Up
+					</a> 
+				</div>
+				</div>
+
+			</form>
+		</div>
+	</div>
+	
